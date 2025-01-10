1265d089335454d432fa8e46c081a4a21c0fb9da
david-garcia1 <davidsgarcia999@gmail.com>
2025-01-10 09:40:50 -0700

    created a navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom"
+
+export default function Navbar() {
+
+    return(
+        <div>
+            <div>
+                <h1>David Garcia</h1>
+                <ul>
+                    <li><NavLink to="/about">About Me</NavLink></li>
+                    <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                    <li><NavLink to="/resume">Resume</NavLink></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
